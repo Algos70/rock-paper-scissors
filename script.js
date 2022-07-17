18213a75da9dc59ce844987d18a525d1a10f22e3
Kursat <draknes.gamin70@gmail.com>
2022-07-17 19:14:23 +0300

    Created playRound Function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Choose a random number with math.random() function then 0 => rock, 1 => paper, 2 => scissor
+//Choose a random number with math.random() function then 0 => rock, 1 => paper, 2 => scissors
 function getComputerChoice() {
     let random = Math.random() * 3;
 
@@ -9,6 +9,34 @@ function getComputerChoice() {
         return "paper";
     }
     else {
-        return "scissor"
+        return "scissors"
     }
+}
+
+// A game of rock paper scissors, done with if else blocks
+function playRound(playerSelection, ComputerSelection) {
+    let playerChoice = "" + playerSelection.toLowerCase();
+
+    if (playerChoice === ComputerSelection) {
+        return `It's a draw!`;
+    }
+    else if (playerChoice === "rock" && ComputerSelection === "paper") {
+        return `You lose! ${ComputerSelection} beats ${playerChoice}`;
+    }
+    else if (playerChoice === "rock" && ComputerSelection === "scissors") {
+        return `You win! ${playerChoice} beats ${ComputerSelection}`;
+    }
+    else if (playerChoice === "paper" && ComputerSelection === "rock") {
+        return `You win! ${playerChoice} beats ${ComputerSelection}`;
+    }
+    else if (playerChoice === "paper" && ComputerSelection === "scissors") {
+        return `You lose! ${ComputerSelection} beats ${playerChoice}`;
+    }
+    else if (playerChoice === "scissors" && ComputerSelection === "paper") {
+        return `You win! ${playerChoice} beats ${ComputerSelection}`;
+    }
+    else if (playerChoice === "scissors" && ComputerSelection === "rock") {
+        return `You lose! ${ComputerSelection} beats ${playerChoice}`;
+    }
+
 }
